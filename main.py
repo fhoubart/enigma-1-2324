51f63e741fdaa05427d0741db3d74aad942d552c
HOUBART FLORENT <fhoubart@auchan.fr>
2023-10-05 15:29:20 +0200

    .

--- main.py
+++ main.py
@@ -4,12 +4,20 @@ print("Hello World!")
 nomVariable = 2  # nomVariable est un entier
 nomVariable = "Bonjour"  # nomVariable est une chaine de caractères
 nomVariable = "2"
+variableBool = True # Type boolean
+
+# Conversion de types
+variableInt = int("2") # variableInt est un entier
+print(variableInt * 2)  # -> 4
+variableStr = str(2)
+print(variableStr * 2) # -> 22
 
 # Nom des variables
 adlh2sgsfgj = 2 # A ne pas faire : mettre des noms qui signifient quelque chose
 # 2 bonnes pratiques pour nommer les variables:
 uneVariableQuiSertAQuelqueChose = 2  # CamelCase
 une_variable_qui_sert_a_quelque_chose = 3  # avec des _
+NOMBRE_DE_JOUEURS = 3 # constante en majuscule
 
 print(nomVariable)
 print(adlh2sgsfgj)
@@ -23,6 +31,19 @@ a = nomVariable + "5"
 b = uneVariableQuiSertAQuelqueChose + 5
 print(a)
 print(b)
+
+# Opérateurs numériques
+# + : addition
+# - : soustraction
+# * : multiplication
+# / : division
+# // : quotion de la division euclidienne
+# % : modulo = reste de la division euclidienne, revient à compter en boucle
+# ** : puissance
+
+# Opérateurs chaines de caractères / listes :
+# + : concaténation
+# * : répétition de chaine
 
 # Autres opérations numériques
 print("Autres opérations numériques")
@@ -46,14 +67,25 @@ if b > 5:
     print("b > 5")
 # fin de l'indentation, on n'est plus dans le if
 
+# If basic
 if b < 5:
     print("b < 5")
 print("fin du if")
 
+# if else
 if b < 5:
     print("b < 5")
 else:
     print("b >= 5 (via else)")
+
+# if avec plusieurs conditions
+if b > 5:
+    print("b>5")
+elif b > 3:
+    print("b est compris entre 3 et 5 (3 non inclus)")
+else:
+    print("b est inferieur ou égal à 3")
+
 
 # Opérateurs de comparaison
 print("Opérateurs de comparaison")
@@ -75,10 +107,11 @@ print("un message text") # afficher une chaine de caractère
 print(b) # affiche une variable, en gérant le type de la variable (si b est un int, il sera converti automatiquement en str)
 print(b, a, "du text")  # print affiche successivement ses différents paramètres séparés par un espace
 print("La variable b est inférieur à 10 (b=" + str(b) + ")")
+print(f'La vaiable b est inférieur à 10 ({b})') # formattage de chaine de caractères
 
 # Gestion des inputs : attention à convertir en entier avec int() si besoin d'un type numérique
-#userInput = int(input("entrez la valeur de départ (entier):"))
-#print(userInput + 3)
+userInput = int(input("entrez la valeur de départ (entier):"))
+print(userInput + 3)
 
 
 # Boucle
@@ -116,6 +149,9 @@ for i in [1, 2, 3, 4, 5]:
 for i in range(1,5):
     print("Iteration " + str(i))
 
+range(5,1,-1) # Range décroissant : 5, 4, 3, 2
+range(1,5,2)  # Range par pas de deux : 1, 3
+range(5, 1, -2) # range décroissant par pas de deux : 5, 3
 
 # Procédure : un ensemble d'instructions à exécuter en blocs
 # Fonctions : renvoie une valeur
@@ -145,19 +181,95 @@ def factorielleRecursif(n):
         return n*factorielleRecursif(n-1)
 
 
+def estPair(x):
+    #if x % 2 == 0:
+    #    return True
+    #else
+    #    return False
+    return x % 2 == 0
 
-sapin(3)
-sapin(10)
+def max(a,b):
+    """
+    Calcule le maximum entre les deux paramètres du même type
+    :param a: la première valeur à comparer, peut importe le type
+    :param b: La deuxième valeur à comparer, qui doit être du même type que a
+    :return: Le max des deux valeurs
+    """
+    if a < b:
+        return b
+    else:
+        return a
 
-# 50 = 4*11 + 6
-print(50 // 11) # 4
-print(50 % 11)  # 6
+max(2,3)
+a = 5
+b = 8
+x = 2
+y = 3
+c = max(x,y)
 
-# 10 = 2*5 + 0
-print(10 // 5)
-print(10 % 5)
+def tousLesPairs(x):
+    for i in range(0, x+1):
+        if estPair(i):
+            print(i)
+
+def tousLesPairs2(x):
+    # On avance par pas de 2, donc tous les i sont pairs
+    for i in range(0,x+1,2):
+        print(i)
 
 
-# Les modules
+
+###########################
+## Liste
+###########################
+# Documentation : https://docs.python.org/3/library/stdtypes.html#sequence-types-list-tuple-range
+
+tableau = ['Pomme', 'Banane', 'Poire', 3, 7]
+
+# accède via des indexes des éléments
+print(tableau[2])  # -> Poire
+print(tableau[0])  # -> Pomme
+
+# Parcourir tout le tableau
+for i in tableau:
+    print(i) # Pomme, puis Banane, puis Poire, puis 3, puis 7
+
+print(tableau)
+print(tableau[0:2])
+print(tableau[0:3])
+print(tableau[0:3:2])
+
+tableau[4] = 'Cerise'
+print(tableau)
+#tableau[5] = 'Fraise' # ne fait rien, le tableau n'a pas d'élément d'index 5
+#print(tableau)
+
+tableau.append("Fraise")
+print(tableau)
+
+# Utiliser les tableau comme des stacks (piles)
+print(tableau.pop())
+print(tableau)
+
+
+if 'Banane' in tableau:
+    print("Il y a une banane dans le tableau, elle se trouve à l'index:")
+    print(tableau.index('Banane'))
+
+chaine = "Hello World"
+print(chaine[1]) # e
+print(chaine[0:5]) # Hello
+# La chaine étant un tableau de lettres, on peut boucler dessus
+for i in chaine:
+    print(i)
+
+tableauEntier = [1, 5, 2, 8, 4, 7, 6]
+tableauEntier.sort()
+print(tableauEntier)
+
 # lien vers la doc python : https://docs.python.org/
 # Aller dans "Library" et chercher ce qui vous interesse
+
+
+
+
